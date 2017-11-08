4289e697d1e8a9efeb5261a70e3358a79006b066
Victoria Meng <nights1ststar@protonmail.com>
2017-11-08 04:47:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,11 +5,9 @@ function lowerCaseDrivers(drivers) {
 }
 
 function nameToAttributes(drivers) {
-  let drivers_object = {}
-
-  drivers.map();
-
-  return drivers_object;
+  return drivers.map(function (driver) {
+    return driver.toLowerCase();
+  });
 
 }
 
