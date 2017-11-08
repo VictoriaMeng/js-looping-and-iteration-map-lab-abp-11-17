c9776776704a2a20f69394f9a4f95f57718c3aaf
Victoria Meng <nights1ststar@protonmail.com>
2017-11-08 04:33:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // Code your solution in this file.
+const drivers = ["Anne"]
+
 function lowerCaseDrivers(drivers) {
   return drivers.map(function (driver) {
     console.log(driver.toLowerCase());
