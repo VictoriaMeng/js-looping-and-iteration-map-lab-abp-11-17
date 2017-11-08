81862ad7d27ad002e1d04d6bd816cdbd0e9c83ab
Victoria Meng <nights1ststar@protonmail.com>
2017-11-08 05:08:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function attributesToPhrase(drivers) {
   let list = "";
 
   drivers.map(function (driver) {
-    list = '${list} + ${driver.name} is from ${driver.hometown}';
+    list = '${driver.name} is from ${driver.hometown}';
   });
 
   return list;
