dd55c2d6369c3e7da0af80c5c41e6db93a79b6cf
Victoria Meng <nights1ststar@protonmail.com>
2017-11-08 04:32:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 // Code your solution in this file.
 function lowerCaseDrivers(drivers) {
   return drivers.map(function (driver) {
-    driver.toLowerCase();
+    console.log(driver.toLowerCase());
   });
 }
-
-
