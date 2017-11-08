346f6a6e5a060d8a6cb7876e85cab1bfd827ebce
Victoria Meng <nights1ststar@protonmail.com>
2017-11-08 05:12:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file.
+function lowerCaseDrivers(drivers) {
+  return drivers.map(function (driver) {
+    return driver.toLowerCase();
+  });
+}
+
+function nameToAttributes(drivers) {
+  const all_names = [];
+
+  drivers.map(function (driver) {
+    const nameObject = {};
+    const names = driver.split(" ");
+    nameObject.firstName = names[0];
+    nameObject.lastName = names[1];
+    all_names.push(nameObject);
+  });
+
+  console.log(all_names);
+
+  return all_names;
+}
+
+function attributesToPhrase(drivers) {
+  let list = [];
+
+  drivers.map(function (driver) {
+    list.push(`${driver.name} is from ${driver.hometown}`)
+  });
+
+  return list;
+}
