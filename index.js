3c1dceba13c9352242be4178e33afb1544ed8329
Victoria Meng <nights1ststar@protonmail.com>
2017-11-08 04:56:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,8 +9,7 @@ function nameToAttributes(drivers) {
 
   drivers.map(function (driver) {
     var names = driver.split(" ");
-    all_names[names[0]] = names[1];
-    console.log(names, all_names);
+
   });
 
   return all_names;
